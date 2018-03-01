debfabc08202b482141ac170202d1b31daf85858
有心猴 <belialofking@163.com>
2018-03-01 08:59:45 +0800

    换个前台模板

--- src/main/java/me/schedule/framework/AuthorityFilter.java
+++ src/main/java/me/schedule/framework/AuthorityFilter.java
@@ -1,6 +1,5 @@
 package me.schedule.framework;
 
-import org.nutz.dao.Cnd;
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
@@ -19,15 +18,12 @@ public class AuthorityFilter implements ActionFilter {
 
     @Inject
     private Dao dao;
-    
-    private static final View INDEX =  new ServerRedirectView("/main");
-    private static final View LOGIN =  new ServerRedirectView("/login");
+
+    private static final View INDEX = new ServerRedirectView("/main");
+    private static final View LOGIN = new ServerRedirectView("/login");
     private static final View ERROR = new JspView("view.error");
 
 
-
-    
-    
     @Override
     public View match(ActionContext ac) {
         String serial = ac.getServletContext().getInitParameter("serial");
@@ -39,22 +35,22 @@ public class AuthorityFilter implements ActionFilter {
         //检查用户登录权限
         Authority ma = ac.getMethod().getAnnotation(Authority.class);
         //没用添加标注不做校验
-        if(ma == null){
+        if (ma == null) {
             Logs.get().info("url不需要权限验证!");
             return null;
         }
         Logs.get().info("校验用户登录开始");
-        Boolean isLogin = (Boolean)Mvcs.getHttpSession().getAttribute(Constant.__IS_LOGIN__);
-        if(!Boolean.TRUE.equals(isLogin)){
+        Boolean isLogin = (Boolean) Mvcs.getHttpSession().getAttribute(Constant.__IS_LOGIN__);
+        if (!Boolean.TRUE.equals(isLogin)) {
             Logs.get().info("用户未登录!");
             return LOGIN;
         }
-        if(Strings.isBlank(ma.value())){
+        if (Strings.isBlank(ma.value())) {
             Logs.get().info("只需要登录验证,无需权限验证!");
             return null;
         }
-        String loginId = (String)Mvcs.getHttpSession().getAttribute(Constant.__LOGIN_ID__);
-        if("super".equals(loginId)){
+        String loginId = (String) Mvcs.getHttpSession().getAttribute(Constant.__LOGIN_ID__);
+        if ("super".equals(loginId)) {
             return null;
         }
 //
--- src/main/java/me/schedule/utils/IdUtil.java
+++ src/main/java/me/schedule/utils/IdUtil.java
@@ -6,34 +6,35 @@ import java.util.concurrent.TimeUnit;
 public class IdUtil {
 
     private String lastTimeStr = "";
-    private int sequence=0;
+    private int sequence = 0;
     static IdUtil instance = new IdUtil();
 
     public static IdUtil getInstance() {
         return instance;
     }
 
-    private void IdUtil(){
+    private void IdUtil() {
     }
-    private synchronized String generateId(){
+
+    private synchronized String generateId() {
         StringBuilder sb = generateDtStr();
-        while(sb.toString().equals(lastTimeStr) && sequence >= 36){
+        while (sb.toString().equals(lastTimeStr) && sequence >= 36) {
             try {
                 TimeUnit.MICROSECONDS.sleep(1);
             } catch (InterruptedException e) {
             }
             sb = generateDtStr();
         }
-        if(!sb.toString().equals(lastTimeStr)){
+        if (!sb.toString().equals(lastTimeStr)) {
             lastTimeStr = sb.toString();
             sequence = 0;
         }
-        sb.append(Integer.toString(sequence,36));
+        sb.append(Integer.toString(sequence, 36));
         sequence++;
         return sb.toString().toUpperCase();
     }
 
-    private StringBuilder generateDtStr(){
+    private StringBuilder generateDtStr() {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR) - 2010;
         int month = cal.get(Calendar.MONTH) + 10;
@@ -43,27 +44,28 @@ public class IdUtil {
         int second = cal.get(Calendar.SECOND);
         int millisecond = cal.get(Calendar.MILLISECOND);
         StringBuilder sb = new StringBuilder();
-        sb.append(Integer.toString(year,36)); //年
-        sb.append(Integer.toString(month,36)); //月
-        sb.append(Integer.toString(day,36)); //
-        sb.append(Integer.toString(hour,36)); //
-        int ms = minute * 60 + second/10;
-        sb.append(rightSubString("00" + Integer.toString(ms,36),2)); //
-        int sm = (second % 10) * 1000 +  millisecond;
-        sb.append(rightSubString("000" + Integer.toString(sm,36),3)); //
+        sb.append(Integer.toString(year, 36)); //年
+        sb.append(Integer.toString(month, 36)); //月
+        sb.append(Integer.toString(day, 36)); //
+        sb.append(Integer.toString(hour, 36)); //
+        int ms = minute * 60 + second / 10;
+        sb.append(rightSubString("00" + Integer.toString(ms, 36), 2)); //
+        int sm = (second % 10) * 1000 + millisecond;
+        sb.append(rightSubString("000" + Integer.toString(sm, 36), 3)); //
         return sb;
     }
 
-    private String rightSubString(String value,int length){
-        if(value.length() == length){
+    private String rightSubString(String value, int length) {
+        if (value.length() == length) {
             return value;
         }
-        if(value.length() < length){
+        if (value.length() < length) {
             return value;
         }
         return value.substring(value.length() - length);
     }
-    public static String nextId(){
+
+    public static String nextId() {
         return instance.generateId();
     }
 
